936d5cf0b090a3ed5561a7fbe649328c658217fd
Corey <aingc@users.noreply.github.com>
2021-01-08 22:20:32 -0800

    testing trailer links

--- src/Row.js
+++ src/Row.js
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 import axios from './axios';
 import './Row.css';
 
@@ -8,6 +10,7 @@ export default function Row(props) {
   const { title, fetchUrl, isLargeRow } = props;
 
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   //rule of thumb, any var used in useEffect, should be in the dependency array
   useEffect(() => {
@@ -19,6 +22,27 @@ export default function Row(props) {
     fetchData();
   }, [fetchUrl]);
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1
+    }
+  }
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl('');
+    } else {
+      movieTrailer(movie?.name || "")
+      .then((url) => {
+        const urlParams = new URLSearchParams(new URL(url).search);
+        setTrailerUrl(urlParams.get('v'));
+      })
+      .catch((err) => console.log(err));
+    }
+  }
+
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -27,12 +51,14 @@ export default function Row(props) {
         {movies.map(movie => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
             alt={movie.title}
             />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   )
 }
